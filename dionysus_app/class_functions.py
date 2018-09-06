fb5fcc9222e94beea448171834fa1f593b6c539f
David <toonarmycaptain@hotmail.com>
2018-09-06 14:52:49 -0500

    Heavy refactoring of classlist creation. TODO: implement path/pathlib for file access to app_data.

--- dionysus_app/class_functions.py
+++ dionysus_app/class_functions.py
@@ -1,27 +1,51 @@
 import os
 
+CLASSLIST_PATH = 'dionysus_app/app_data/class_data/'
+
 def create_classlist():
 
+    classlist_name = take_classlist_name_input()
 
-    classlist_name = input('Please enter a name for the class: ')
-    classlist_name = classlist_name.replace(' ', '_')
-    while os.path.exists(classlist_name + '.cld'):  # TODO: Make path point at data folder. .cll meaning. classlist
-        print('A class with this name already exists.')
-        input('Please try another class name: ')
 
-    with open(classlist_name + '.cld', 'w+') as classlist_file:  # TODO: Make path point at data folder.
+    with open('dionysus_app/app_data/class_data/' + classlist_name + '.cld', 'w+') as classlist_file:  # TODO: Make path point at data folder.
         while True:
+            student_names = ''
             student_name = input("Enter student name, or 'end': ")
-            if False: # TODO search for it in class - if it exists, ask for more input
+            if student_name in student_names: # TODO search for it in class - if it exists, ask for more input
                 print("This student is already a member of the class.")
                 continue
             elif student_name.upper() =='END':
                 break
             else:
-                classlist_file.write(student_name+'\n')
+                student_names += (student_name+'\n')
+        classlist_file.write(student_names)
+
+
+def take_classlist_name_input():
+
+    while True:
+        classlist_name = input('Please enter a name for the class: ')
+        classlist_name = classlist_name.replace(' ', '_')
+
+        if classlist_name == '':  # blank input
+            continue
+
+        if classlist_exists(classlist_name):
+            print('A class with this name already exists.')
+            continue
+        break
+    return classlist_name
 
 
 
-if __name__ == '__main__'
+
+def classlist_exists(classlist_name):
+    if os.path.exists('app_data/class_data/' + classlist_name + '.cld'):  # TODO: Make path point at data folder. .cld meaning. ClassListData
+        return True
+
+
+
+
+if __name__ == '__main__':
     create_classlist()
     # same for edit classlist except for with open(classlist_name + '.txt', 'r+') as classlist_file:
