c4e1ba8bf3046d8dc885c20d68302b8a9866b1b1
David <toonarmycaptain@hotmail.com>
2018-09-17 14:56:42 -0500

    Add setup.py, add to travis.yml

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='dionysus_app',
+      # version='0.0.1',
+      description='Avatar graph generator',
+      author='David Antonini',
+      author_email='toonarmycaptain@hotmail.com',
+      url='https://github.com/toonarmycaptain/dionysus',
+      packages=find_packages(),
+      license='GNU Affero General Public License',
+      long_description=open('README.txt').read(),
+     )
