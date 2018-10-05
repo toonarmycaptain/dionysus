3d4822248b5785a12123656f08e4518f0291b8e7
David <toonarmycaptain@hotmail.com>
2018-10-04 22:05:12 -0500

    Add Path import, use OS agnostic Path.cwd.as_uri() over os.getcwd()

--- test_suite/test_data_folder.py
+++ test_suite/test_data_folder.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from pathlib import Path
 from dionysus_app.data_folder import DataFolder
 
 
@@ -14,10 +15,12 @@ class TestDataFolder(unittest.TestCase):
 
     def test_generate_data_path_defaults(self):
         os.chdir(os.path.join(os.getcwd(), '..'))
+        cwd_path = Path.cwd().as_uri()  # cwd path OS agnostic for assertion
         for path in self.default_paths:
             path_result = DataFolder.generate_rel_path(path).as_uri()
-            assert path in path_result and os.getcwd() in path_result
+            assert path in path_result and cwd_path in path_result
 
     def test_generate_data_path_None(self):
         path_result = DataFolder.generate_rel_path(None)
-        assert os.getcwd() in path_result
+        cwd_path = Path.cwd().as_uri()  # cwd path OS agnostic for assertion
+        assert cwd_path in path_result
