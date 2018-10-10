cdcc3dfcc1f1f914f19399cb2f903dc01f7f10cb
Piotr Czajka <piotr_czajka@outlook.com>
2018-10-10 11:25:09 +0200

    Made basic functional test for class creator

---
+++ test_suite/test_create_classlist.py
@@ -0,0 +1,26 @@
+import signal
+from unittest import TestCase, mock
+
+import pexpect.popen_spawn
+
+
+class TestClassList(TestCase):
+
+    def setUp(self):
+        self.child = pexpect.spawn('python app_main.py')
+        self.child.expect('1. Create a classlist', timeout=2)
+        self.child.sendline('1')
+
+    def tearDown(self):
+        self.child.kill(signal.SIGKILL)
+
+
+    def test_can_create_empty_class(self):
+        self.child.expect('Please enter a name for the class', timeout=2)
+        self.child.sendline('fooclass')
+        self.child.expect("Enter student name, or 'end':", timeout=2)
+        self.child.sendline('end')
+        self.child.expect('Do you want to create an empty class?', timeout=2)
+        self.child.sendline('y')
+        self.child.expect('Class: fooclass', timeout=2)
+        self.child.expect('No students entered', timeout=2)
