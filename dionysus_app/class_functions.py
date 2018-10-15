3cd6e165483aceb793a3db86c51277064a12dbc7
toonarmycaptain <toonarmycaptain@hotmail.com>
2018-10-14 23:18:48 -0500

    Reverse classed class_functions.py commit
    Reverse classed class_functions.py commit by updating to current status.

--- dionysus_app/class_functions.py
+++ dionysus_app/class_functions.py
@@ -1,7 +1,8 @@
 """
-Class Creation utility
+Functions for creating, editing, dealing with classes.
 """
 
+import json
 import time
 from pathlib import Path
 
@@ -13,210 +14,250 @@ CLASS_REGISTRY_PATH = DataFolder.generate_rel_path(DataFolder.CLASS_REGISTRY.val
 CLASSLIST_DATA_FILE_TYPE = '.cld'
 
 
-class ClassCreator:
+def create_classlist():
 
-    def proceed_to_class_setup(self):
+    classlist_name = take_classlist_name_input()  # TODO: Option to cancel creation at class name entry stage
 
-        class_name = self.__get_class_name_from_user()  # TODO: Option to cancel creation at class name entry stage
-
-        self.__setup_class(class_name)
-        self.__create_classlist_data(class_name)
+    setup_class(classlist_name)
+    create_classlist_data(classlist_name)
 
 
-    def __create_classlist_data(self, classlist_name):  # TODO: fix path composition
-        data_file = classlist_name + CLASSLIST_DATA_FILE_TYPE
-        classlist_data_path = CLASSLIST_DATA_PATH.joinpath(classlist_name, data_file)
+def create_classlist_data(class_name: str):
 
-        with open(classlist_data_path, 'w+') as classlist_file:
-            cancelled = False
-            while True:
-                class_data = self.__take_class_data_input()
+    class_data = compose_classlist_dialogue()
 
-                if class_data == '':  # Test for empty class.
-                    cancelled = self.__blank_class_dialogue()
-                    if cancelled:
-                        break
-                    # else: ie if not cancelled:
-                    continue
-                break  # class_data not empty
+    class_data_feedback(class_name, class_data)
+    write_classlist_to_file(class_name, class_data)
+    time.sleep(2)  # Pause for user to look over feedback.
 
-            print(f'\nClass: {classlist_name}')
+
+def compose_classlist_dialogue():
+    while True:
+        class_data = take_class_data_input()
+
+        if not class_data:  # Test for empty class.
+            cancelled = blank_class_dialogue()
             if cancelled:
-                print("No students entered.")
-            else:
-                print(class_data)
-
-            classlist_file.write(class_data)  # consider using JSON?
-            time.sleep(2)
-
-
-    def __blank_class_dialogue(self):
-        while True:
-            choice = input("Do you want to create an empty class? y/n")
-            if choice.upper() == 'Y':
-                return True
-            if choice.upper() == 'N':
-                return False
-            # TODO: Option to cancel creation here/after entering a class name (eg made typo in class name)
-            print('Please enter y for yes to create empty class, or n to return to student input.')
-
-
-    def __take_class_data_input(self):
-        """
-        Continues to get pairs of values (student name, avatar filename) unless user type 'end'
-
-        :return: str
-        """
-        class_data = ''
-        while True:
-
-            student_name = self.__take_student_name_input(class_data)
-            if student_name.upper() == 'END':
                 break
+            # else: ie if not cancelled:
+            continue
+        break  # class_data not empty
 
-            avatar_filename = self.__take_student_avatar(student_name)
-            # else:
-            class_data += f'{student_name}, {avatar_filename}\n'  # consider using JSON? dictionaries?
-        return class_data
-
-
-    def __take_student_name_input(self, class_data):
-        """
-        Prompts user for student name.
-
-        :param class_data: str
-        :return: str
-        """
-        while True:
-            student_name = input("Enter student name, or 'end': ")
-            if input_is_essentially_blank(student_name):  # Do not allow blank input TODO: include dash, underscore
-                print('Please enter a valid student name.')
-                continue
-
-            if student_name in class_data:  # TODO: search for it in class - if it exists, ask for more input
-                print("This student is already a member of the class.")
-                continue
-            return student_name
+    return class_data
 
 
-    def __take_student_avatar(self, student_name):
-        """
-        Prompts user for path to avatar file.
+def blank_class_dialogue():
+    while True:
+        choice = input("Do you want to create an empty class? y/n")
+        if choice.upper() == 'Y':
+            return True
+        if choice.upper() == 'N':
+            return False
+        # TODO: Option to cancel creation here/after entering a class name (eg made typo in class name)
+        print('Please enter y for yes to create empty class, or n to return to student input.')
 
-        :param student_name: str
-        :return: str or None
-        """
-        print(f'Load avatar image for {student_name}.')
-        while True:
-            avatar_file = input(r'Please paste complete filepath and name eg C:\my_folder\my_avatar.jpg or None to skip: ')
-            if avatar_file.upper() == 'NONE':
-                return None
-            if self.__avatar_file_exists(avatar_file):
-                break
-            # else:
-            print('Supplied filepath cannot be found.')
 
-        if avatar_file is None:
+def take_class_data_input():
+    """
+    Take student names, avatars, return dictionary of data.
+
+    :return: dict
+    """
+    class_data = {}
+    while True:
+        student_name = take_student_name_input(class_data)
+        if student_name.upper() == 'END':
+            break
+        avatar_filename = take_student_avatar(student_name)
+        class_data[student_name] = [avatar_filename]
+    return class_data
+
+
+def take_student_name_input(class_data):
+    """
+    Prompts user for student name.
+
+    :param class_data: str
+    :return: str
+    """
+    while True:
+        student_name = input("Enter student name, or 'end': ")
+        if input_is_essentially_blank(student_name):  # Do not allow blank input
+            print('Please enter a valid student name.')
+            continue
+
+        if student_name in class_data:
+            print("This student is already a member of the class.")
+            continue
+        return student_name
+
+
+def take_student_avatar(student_name):
+    """
+    Prompts user for path to avatar file.
+
+    :param student_name: str
+    :return: str or None
+    """
+    print(f'Load avatar image for {student_name}.')
+    while True:
+        avatar_file = input(r'Please paste complete filepath and name eg C:\my_folder\my_avatar.jpg or None to skip: ')
+        if avatar_file.upper() == 'NONE':
             return None
-        cleaned_student_name = clean_for_filename(student_name)
-        avatar_filename = f'{cleaned_student_name}.jpg'
-        # process_student_avatar()
-        # convert to jpg or whatever, copy image file to class_data avatar folder with filename that is student name
-        return avatar_filename
+        if avatar_file_exists(avatar_file):
+            break
+        # else:
+        print('Supplied filepath cannot be found.')
+
+    if avatar_file is None:
+        return None
+    cleaned_student_name = clean_for_filename(student_name)
+    avatar_filename = f'{cleaned_student_name}.jpg'
+    # process_student_avatar()
+    # convert to jpg or whatever, copy image file to class_data avatar folder with filename that is student name
+    return avatar_filename
 
 
-    def __setup_class(self, classlist_name):  # TODO: change name because of class with python 'class' keyword?
-        """
-        Setup class data storage file structure.
-        Register class in class_registry index
+def class_data_feedback(classlist_name: str, class_data_dict: dict):
+    """
+    Print classlist name and list of students as user feedback.
 
-        :param classlist_name:
-        :return:
-        """
-        self.__setup_class_data_storage(classlist_name)
-        self.__register_class(classlist_name)
-
-
-    def __setup_class_data_storage(self, classlist_name):
-        """
-        Setup data storage for new classes.
-
-        Structure for data storage:
-        app_data/
-            class_data/
-                class_name/  # folder for each class
-                    graph_data/  # store graph data sets
-                    avatars/  # store avatars for class
+    :param classlist_name: str
+    :param class_data_dict: dict
+    :return: None
+    """
+    print(f'\nClass name: {classlist_name}')
+    if not class_data_dict:
+        print("No students entered.")
+    else:
+        for key in class_data_dict:
+            print(key)
 
 
-        :param classlist_name: str
-        :return: None
-        """
-        avatar_path = CLASSLIST_DATA_PATH.joinpath(classlist_name, 'avatars')
-        graph_path = CLASSLIST_DATA_PATH.joinpath(classlist_name, 'graph_data')
+def write_classlist_to_file(class_name: str, class_data_dict: dict):
+    """
+    Write classlist data to disk with format:
 
-        avatar_path.mkdir(exist_ok=True, parents=True)
-        graph_path.mkdir(exist_ok=True, parents=True)
+    Classlist name
+    JSON'd class data dict  # Second line, when reading JSON back in.
+
+    :param class_name: str
+    :param class_data_dict: dict
+    :return: None
+    """
+    data_file = class_name + CLASSLIST_DATA_FILE_TYPE
+    classlist_data_path = CLASSLIST_DATA_PATH.joinpath(class_name, data_file)
+
+    json_class_data = convert_to_json(class_data_dict)
+
+    with open(classlist_data_path, 'w') as classlist_file:
+        classlist_file.write(f'{class_name}\n')
+        classlist_file.write(json_class_data)
 
 
-    def __register_class(self, classlist_name):
-        """
-        Register class in class_registry file.
-        Create if registry non-existent.
+def convert_to_json(data_to_convert):
+    """
+    Serialise data in JSON format, return as JSON string.
 
-        :param classlist_name: str
-        :return: None
-        """
-        with open(CLASS_REGISTRY_PATH, 'a+') as class_registry:  # open class registry, create if does not exist.
-            class_registry.write(f'{classlist_name}\n')
+    :param data_to_convert:
+    :return: str
+    """
+    converted_data = json.dumps(data_to_convert, indent=4)
+    return converted_data
 
 
-    def __avatar_file_exists(self, avatar_file):
-        """
-        Chcecks if provided file exists.
+def setup_class(classlist_name):  # TODO: change name because of class with python 'class' keyword?
+    """
+    Setup class data storage file structure.
+    Register class in class_registry index
 
-        :param avatar_file: str
-        :return: bool
-        """
-        return Path(avatar_file).expanduser().resolve().exists()
+    :param classlist_name:
+    :return:
+    """
+    setup_class_data_storage(classlist_name)
+    register_class(classlist_name)
+
+
+def setup_class_data_storage(classlist_name):
+    """
+    Setup data storage for new classes.
+
+    Structure for data storage:
+    app_data/
+        class_data/
+            class_name/  # folder for each class
+                graph_data/  # store graph data sets
+                avatars/  # store avatars for class
+
+
+    :param classlist_name: str
+    :return: None
+    """
+    avatar_path = CLASSLIST_DATA_PATH.joinpath(classlist_name, 'avatars')
+    graph_path = CLASSLIST_DATA_PATH.joinpath(classlist_name, 'graph_data')
+
+    avatar_path.mkdir(exist_ok=True, parents=True)
+    graph_path.mkdir(exist_ok=True, parents=True)
+
+
+def register_class(classlist_name):
+    """
+    Register class in class_registry file.
+    Create if registry non-existent.
+
+    :param classlist_name: str
+    :return: None
+    """
+    with open(CLASS_REGISTRY_PATH, 'a+') as class_registry:  # open class registry, create if does not exist.
+        class_registry.write(f'{classlist_name}\n')
+
+
+def avatar_file_exists(avatar_file):
+    """
+    Checks if provided file exists.
+
+    :param avatar_file: str
+    :return: bool
+    """
+    return Path(avatar_file).expanduser().resolve().exists()
 
 
 # TODO: reorder/rearrange functions
 
 
-    def __get_class_name_from_user(self):
-        """
-        Prompts user for classlist name.
-        It repeats until user provide correct classlist name.
+def take_classlist_name_input():
+    """
+    Prompts user for classlist name.
+    It repeats until user provide correct classlist name.
 
-        :return: str
-        """
+    :return: str
+    """
 
-        while True:
-            classlist_name = input('Please enter a name for the class: ')
+    while True:
+        classlist_name = input('Please enter a name for the class: ')
 
-            if input_is_essentially_blank(classlist_name):  # blank input
-                continue
+        if input_is_essentially_blank(classlist_name):  # blank input
+            continue
 
-            classlist_name = clean_for_filename(classlist_name)
-            if self.__classlist_exists(classlist_name):
-                print('A class with this name already exists.')
-                continue
-            break
-        return classlist_name
+        classlist_name = clean_for_filename(classlist_name)
+        if classlist_exists(classlist_name):
+            print('A class with this name already exists.')
+            continue
+        break
+    return classlist_name
 
 
-    def __classlist_exists(self, classlist_name):
-        """
-        Checks if there is .cld file for classlist.
+def classlist_exists(classlist_name):
+    """
+    Checks if there is .cld file for classlist.
 
-        :param classlist_name: str
-        :return: bool
-        """
-        classlist_file_path = Path(classlist_name, CLASSLIST_DATA_FILE_TYPE)
-        return CLASSLIST_DATA_PATH.joinpath(classlist_file_path).exists()
+    :param classlist_name: str
+    :return: bool
+    """
+    classlist_file_path = Path(classlist_name, CLASSLIST_DATA_FILE_TYPE)
+    return CLASSLIST_DATA_PATH.joinpath(classlist_file_path).exists()
 
 
 if __name__ == '__main__':
-    creator = ClassCreator()
-    creator.create_classlist()
+    create_classlist()
+    # similarly for edit classlist - with open(classlist_name + '.txt', 'r+') as classlist_file: to edit
