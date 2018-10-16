05c31cac049213ecd3131a656d01a1eca5c9e2fe
David <toonarmycaptain@hotmail.com>
2018-10-16 15:01:37 -0500

    Add functions generate_registry_from_filesystem, write_registry_to_disk. Integrate with other functions, add import.

--- dionysus_app/class_registry.py
+++ dionysus_app/class_registry.py
@@ -4,30 +4,35 @@ Functions dealing with the class registry.
 
 from dionysus_app.data_folder import DataFolder
 
+CLASSLIST_DATA_PATH = DataFolder.generate_rel_path(DataFolder.CLASS_DATA.value)
 CLASS_REGISTRY_PATH = DataFolder.generate_rel_path(DataFolder.CLASS_REGISTRY.value)
 CLASSLIST_DATA_FILE_TYPE = '.cld'
 
-def cache_class_registry():
-# TODO: if the file structure already exists, check for previously created classes
-    # Check for a class_registry.index in app_data directory
-    #     If list exists, compare with folders (? or .cld files ?) within class_data.
-    #         Use pathlib.iterdir() - https://docs.python.org/3.4/library/pathlib.html#basic-use
-    #                               - https://stackoverflow.com/a/44228436/7942600
-    #     Else check for classes, create class_registry.
-    cached_class_registry = generate_registry_from_filesystem()
-    return cached_class_registry
 
+def cache_class_registry():
 
     global CLASS_REGISTRY
     CLASS_REGISTRY = generate_registry_from_filesystem()
+    write_registry_to_disk(CLASS_REGISTRY)
 
     return CLASS_REGISTRY  # return value unused on startup
 
 
 def generate_registry_from_filesystem():
-
-        registry_list =[x.stem for x in CLASSLIST_DATA_PATH.rglob(f'**/*{CLASSLIST_DATA_FILE_TYPE}')]
+        registry_list = [x.stem for x in CLASSLIST_DATA_PATH.rglob(f'**/*{CLASSLIST_DATA_FILE_TYPE}')]
         return registry_list
+
+
+def write_registry_to_disk(registry_list: list):
+    """
+    Write registry list from cache list to disk.
+
+    :param registry_list: list
+    :return: None
+    """
+    with open(CLASS_REGISTRY_PATH, 'w') as class_registry:
+        for classlist_name in registry_list:
+            class_registry.write(f'{classlist_name}\n')
 
 
 def register_class(classlist_name):
@@ -38,5 +43,13 @@ def register_class(classlist_name):
     :param classlist_name: str
     :return: None
     """
+    global CLASS_REGISTRY
+
+    CLASS_REGISTRY.append(classlist_name)
+
     with open(CLASS_REGISTRY_PATH, 'a+') as class_registry:  # open class registry, create if does not exist.
         class_registry.write(f'{classlist_name}\n')
+
+
+if __name__ == '__main__':
+    print(cache_class_registry())
