375b02d23d0a21fa62f108866a80970e5d942802
David <toonarmycaptain@hotmail.com>
2018-10-16 21:37:36 -0500

    Add draft outline of tests for class_registry

---
+++ test_suite/test_class_registry.py
@@ -0,0 +1,28 @@
+import unittest
+
+from dionysus_app.class_registry import register_class
+
+class TestAddClassToNewRegistry(unittest.TestCase):
+    # setup:
+    #     dummy CLASS_REGISTRY_PATH?
+    #     dummy classlist_name
+    #
+    # test add class to registry
+    # confirm registry created
+    # confirm classlist_name in cached registry dict
+    # confirm classlist_name in registry file
+
+class TestAddClassExistingRegistry(unittest.TestCase):
+    # setup:
+    #     dummy CLASS_REGISTRY_PATH?
+    #     dummy existing class registry
+    #     dummy classlist_name
+    #
+    # test add class to registry
+    # confirm classlist_name in cached registry dict
+    # confirm classlist_name in registry file
+
+
+
+
+
