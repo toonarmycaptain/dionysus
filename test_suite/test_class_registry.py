404345c0a9030acf5762d18c46dd172d3fbbd283
David <toonarmycaptain@hotmail.com>
2018-10-31 14:55:04 -0500

    Flesh out plan for tests.

--- test_suite/test_class_registry.py
+++ test_suite/test_class_registry.py
@@ -3,13 +3,36 @@ import unittest
 from dionysus_app.class_registry import register_class
 
 
-class TestLoadNonexistentRegistry(unittest.TestCase):
+class TestLoadNonexistentRegistryWithNoClassData(unittest.TestCase):
     pass
+    # setup:
+    #     dummy CLASS_REGISTRY_PATH?
+    #
+    # confirm blank registry created on startup when no data exists
+    # confirm empty cached registry dict/global
 
 
 class TestLoadExistingRegistry(unittest.TestCase):
     pass
+    # setup:
+    #     dummy CLASS_REGISTRY_PATH?
+    #     dummy class data (existent classes)
+    #
+    # confirm registry file created on startup
+    # confirm registry dict/global created on startup
+    # confirm created registry file has correct data
+    # confirm created registry dict/global has correct data
 
+
+class TestLoadNonexistentRegistryWithClassData:
+    pass
+    # setup:
+    #     dummy CLASS_REGISTRY_PATH?
+    #     dummy class data (existent classes)
+    #
+    # confirm registry file created on startup
+    # confirm registry dict/global created on startup
+    # confirm created registry file has correct data
 
 class TestAddClassToNewRegistry(unittest.TestCase):
     pass
