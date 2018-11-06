0010e636c23ad006e4c8c5edb98b4cf104d77047
David <toonarmycaptain@hotmail.com>
2018-11-06 15:16:30 -0600

    Add save chart data first pass.

--- dionysus_app/graph_generator/take_graph_data.py
+++ dionysus_app/graph_generator/take_graph_data.py
@@ -5,10 +5,12 @@ Script for taking and saving data for graph.
 
 # score entry:
 
-import json
 
-# from dionysus_app.data_folder import CHART_DATA_FILE_TYPE
-# from dionysus_app.file_functions import convert_to_json
+from dionysus_app.data_folder import CHART_DATA_FILE_TYPE, DataFolder
+from dionysus_app.file_functions import convert_to_json
+
+IMAGE_DATA_PATH = DataFolder.generate_rel_path(DataFolder.IMAGE_DATA.value)
+
 
 def take_score_entry(minimum=0, maximum=100):
     """
@@ -35,7 +37,43 @@ def take_score_entry(minimum=0, maximum=100):
         return score_float
 
 
+def write_chart_data_to_file(chart_name: str,
+                             score_data_dict: dict,
+                             chart_data_dict: dict):
+    """
+    ### chart_name, pass score_data, chart_data as a dict or tuple?
+    ### include class name in chart name as enforced format? eg class_name - chart name
+
+
+
+    Write classlist data to disk with format:
+
+    Chart name
+    Classlist name
+    JSON'd class data dict  # Second line, when reading JSON back in.
+        dict keys: student_names, values: scores, None for no score.
+
+    JSON'd graph settings/options dict # Third line of file
+        dict keys: settings varying from default, values: set values
+
+    :param chart_name:
+    :param score_data_dict:
+    :param chart_data_dict:
+    :return:
+    """
+    chart_data_file = chart_name + CHART_DATA_FILE_TYPE
+    chart_data_path = IMAGE_DATA_PATH.joinpath(chart_name, chart_data_file)
+    # TODO: rename 'image' and 'graph' to 'chart' where they appear,
+    # apart from where 'image' is correct (ie referring to actual image/image file.
+
+    json_score_data = convert_to_json(score_data_dict)
+    json_chart_data = convert_to_json(chart_data_dict)
+
+    with open(chart_data_path, 'w') as chart_data_file:
+        chart_data_file.write(f'{chart_name}\n')
+        chart_data_file.write(json_score_data)
+        chart_data_file.write(json_chart_data)
 
 
 if __name__ == '__main__':
-    take_score_entry()  # Do not alter this as it is used in testing.
+    pass
