86d884ecde29e5e12c7f9d16e64e2e3255f94b92
David <toonarmycaptain@hotmail.com>
2018-11-12 22:41:10 -0600

    Split long inline string into two.

--- dionysus_app/class_functions.py
+++ dionysus_app/class_functions.py
@@ -148,7 +148,8 @@ def take_student_avatar(student_name):
     """
     print(f'Load avatar image for {student_name}:')
     while True:
-        avatar_file = input('Please paste complete filepath and name \neg C:\\my_folder\\my_avatar.jpg or None to skip: ')
+        avatar_file = input('Please paste complete filepath and name \n'
+                            'eg C:\\my_folder\\my_avatar.jpg or None to skip: ')
         if avatar_file.upper() == 'NONE':
             return None
         if avatar_file_exists(avatar_file):
