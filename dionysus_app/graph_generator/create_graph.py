500eb0edf406cc98b020cb27d0ae628afbde876d
David <toonarmycaptain@hotmail.com>
2018-11-15 14:56:06 -0600

    Add call for chart_name, pass data to generate_chart_image as dict.

--- dionysus_app/graph_generator/create_graph.py
+++ dionysus_app/graph_generator/create_graph.py
@@ -9,7 +9,7 @@ sort of overlap without obscuring the avatars, or two columns of avatars in a po
 """
 from dionysus_app.class_functions import select_classlist
 from dionysus_app.data_folder import CHART_DATA_FILE_TYPE, DataFolder
-from dionysus_app.graph_generator.take_graph_data import take_score_data
+from dionysus_app.graph_generator.take_graph_data import take_chart_name, take_score_data
 from dionysus_app.file_functions import convert_to_json
 from dionysus_app.UI_functions import clean_for_filename
 
@@ -36,8 +36,16 @@ def new_graph():
 
     student_scores: dict = take_score_data(class_name)
 
+    chart_name = take_chart_name()
 
-def write_chart_data_to_file(class_name: str, chart_data_dict: dict):
+    chart_data_dict = {'class_name': class_name, 'chart_name': chart_name, 'score-avatar_dict': student_scores}
+
+    # chart options here or before score entry, setting chart params, min, max scores etc
+
+    generate_chart_image(chart_data_dict)
+
+
+def write_chart_data_to_file(chart_data_dict: dict):
     """
     ### chart_name, pass score_data, chart_data as a dict or tuple?
     ### include class name in chart name as enforced format? eg class_name - chart name
@@ -60,16 +68,20 @@ def write_chart_data_to_file(class_name: str, chart_data_dict: dict):
     :param chart_data_dict: dict
     :return:
     """
-    chart_filename = clean_for_filename(chart_data_dict[chart_name])
+    chart_filename = clean_for_filename(chart_data_dict['chart_name'])
     chart_data_file = chart_filename + CHART_DATA_FILE_TYPE
-    chart_data_path = CLASSLIST_DATA_PATH.joinpath(class_name, 'graph_data', chart_data_file)
+    chart_data_filepath = CLASSLIST_DATA_PATH.joinpath(chart_data_dict['class_name'], 'graph_data', chart_data_file)
 
     json_chart_data = convert_to_json(chart_data_dict)
 
-    with open(chart_data_path, 'w') as chart_data_file:
+    with open(chart_data_filepath, 'w') as chart_data_file:
         chart_data_file.write(json_chart_data)
 
 
+def generate_chart_image(chart_data):
+    pass
+
 
 if __name__ == '__main__':
+    new_graph()
     pass
