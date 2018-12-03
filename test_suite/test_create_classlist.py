7f36849072169555023e44224e072fcd9c846b1b
David <toonarmycaptain@hotmail.com>
2018-12-03 15:45:05 -0600

    Increase allowed timeout to 3 to prevent (local) failing tests.

--- test_suite/test_create_classlist.py
+++ test_suite/test_create_classlist.py
@@ -8,18 +8,18 @@ class TestClassList(TestCase):
 
     def setUp(self):
         self.child = pexpect.popen_spawn.PopenSpawn('python app_main.py')
-        self.child.expect('1. Create a classlist', timeout=2)
+        self.child.expect('1. Create a classlist', timeout=3)
         self.child.sendline('1')
 
     def tearDown(self):
         self.child.kill(signal.SIGINT)  # TODO: remove created fooclass files after test.
 
     def test_can_create_empty_class(self):
-        self.child.expect('Please enter a name for the class:', timeout=2)
+        self.child.expect('Please enter a name for the class:', timeout=3)
         self.child.sendline('fooclass')  # NB this will fail on subsequent runs when run locally.
-        self.child.expect("Enter student name, or 'end':", timeout=2)
+        self.child.expect("Enter student name, or 'end':", timeout=3)
         self.child.sendline('end')
-        self.child.expect('Do you want to create an empty class?', timeout=2)
+        self.child.expect('Do you want to create an empty class?', timeout=3)
         self.child.sendline('y')
         self.child.expect('Class name: fooclass', timeout=5)
         self.child.expect('No students entered', timeout=5)
