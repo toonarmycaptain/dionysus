ee2b9d547ab63805b737adfc1dae453110222349
David <toonarmycaptain@hotmail.com>
2018-12-03 14:54:42 -0600

    generate_avatar_coords to take custom_chart_params value or default. Use chart_params dict for default values for avatar xy coords generation unless custom chart options used and passed to function.

--- dionysus_app/graph_generator/process_chart_data.py
+++ dionysus_app/graph_generator/process_chart_data.py
@@ -3,8 +3,24 @@ Process input data for image generation code.
 """
 
 
-def generate_avatar_coords(score_avatars_dict):
+chart_params = None  # dict of default params, information for how to set coords for default
+
+
+def generate_avatar_coords(score_avatar_dict, custom_chart_params=None):  # set chart params to a default?
+    """
+    score_avatar_dict - dict of scores to lists of avatar image locations for each score.
+
+    :param score_avatar_dict: dict
+    :param custom_chart_params: dict
+    :return:
+    """
     # take chart_data_dict['score-avatar_dict'] and translate into dict
     # with keys: avatar path, values: list of x, y tuples eg [(80, 80), (90, 90)]
 
+    # chart_data_dict['chart_params'] - dict of any chart option settings etc.
+    #
+    if custom_chart_params:
+        chart_param = custom_chart_params  # or pull values from default chart_param dict
+
     pass
+    # return avatar_coord_dict
