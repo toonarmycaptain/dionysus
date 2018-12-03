1d241cd66be709e8ad510c12f82ec139e637c7f6
David <toonarmycaptain@hotmail.com>
2018-12-03 14:51:46 -0600

    Move generate_avatar_coords to process_chart_data.py.

--- dionysus_app/graph_generator/process_chart_data.py
+++ dionysus_app/graph_generator/process_chart_data.py
@@ -2,3 +2,9 @@
 Process input data for image generation code.
 """
 
+
+def generate_avatar_coords(score_avatars_dict):
+    # take chart_data_dict['score-avatar_dict'] and translate into dict
+    # with keys: avatar path, values: list of x, y tuples eg [(80, 80), (90, 90)]
+
+    pass
