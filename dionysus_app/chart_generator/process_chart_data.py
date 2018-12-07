7e501a5b0780e0a90a90181813c15c204007a1d6
David <toonarmycaptain@hotmail.com>
2018-12-07 15:37:10 -0600

    Add default chart_params values, refactor assign_avatar_ccords.
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- dionysus_app/chart_generator/process_chart_data.py
+++ dionysus_app/chart_generator/process_chart_data.py
@@ -2,15 +2,17 @@
 Process input data for image generation code.
 """
 DEFAULT_CHART_PARAMS = {'column_max_avatars': 10,  # max number of avatars vertically.
-                                                   # Nominally pixels from x-axis to top of image//height of avatar.
-                        'column_width': None,
+                        # Nominally pixels from x-axis to top of image//height of avatar.
+                        'avatar_horizontal_offset': 5,  # Spacing between avatars ~ width of avatar
+                        'avatar_vertical_offset': 10,  # Spacing between avatars ~ height of avatar
+                        'init_vertical_offset': 5,  # Initial offset from x-axis ~ half width of avatar
                         'min_score': 0,
                         'max_score': 100,
                         # TODO: put avatar offsets here
                         }
 
 
-def generate_avatar_coords(score_avatar_dict: dict, chart_params: dict=None):  # set chart params to a default?
+def generate_avatar_coords(score_avatar_dict: dict, chart_params: dict = None):  # set chart params to a default?
     """
     score_avatar_dict - dict of scores to lists of avatar image locations for each score.
 
@@ -25,14 +27,14 @@ def generate_avatar_coords(score_avatar_dict: dict, chart_params: dict=None):  #
     # chart_data_dict['chart_params'] - dict of any chart option settings etc.
     #
     if not chart_params:
-        chart_param = DEFAULT_CHART_PARAMS  # pull values from DEFAULT_CHART_PARAMS dict
+        chart_params = DEFAULT_CHART_PARAMS  # pull values from DEFAULT_CHART_PARAMS dict
 
     # Re-sort in ascending score order.
     score_avatar_dict = {score: score_avatar_dict[score] for score in sorted(score_avatar_dict)}
 
     banded_avatars = assign_avatars_to_bands(score_avatar_dict)  # TODO: use DEFAULT_CHART_PARAMS values for offsets.
-  
-    avatar_coord_dict = assign_avatar_coords(banded_avatars)
+
+    avatar_coord_dict = assign_avatar_coords(banded_avatars, chart_params)
 
     return avatar_coord_dict
 
@@ -66,7 +68,7 @@ def assign_avatars_to_bands(score_avatar_dict: dict):
     return band_dict
 
 
-def assign_avatar_coords(band_avatar_dict):
+def assign_avatar_coords(band_avatar_dict, chart_params=DEFAULT_CHART_PARAMS):
     # iterate over avatars in lists from lowest to highest
     # if more than one column, place in left-most column first so highest scores are rightmost
 
@@ -77,31 +79,88 @@ def assign_avatar_coords(band_avatar_dict):
     # next(y_image_coords)  # yields 4, 11, 18, 25...each time it is called.
 
     avatar_xy_dict = {}
+    col_max_avatars, init_vert_offset, horiz_offset, vert_offset = (chart_params['column_max_avatars'],
+                                                                    chart_params['init_vertical_offset'],
+                                                                    chart_params['avatar_horizontal_offset'],
+                                                                    chart_params['avatar_vertical_offset'],
+                                                                    )
 
     for band in band_avatar_dict.keys():  # iterate over bands in band_avatar_dict
-        if len(band_avatar_dict[band]) < 10:
-            for avatar in range(len(band_avatar_dict[band])):
-                xy = (band, 5 + avatar*10)
-                avatar_loc = band_avatar_dict[band][avatar]
-                # add xy coord to list of coords for avatar
-                avatar_xy_dict[avatar_loc] = avatar_xy_dict.get(avatar_loc, []) + [xy]
+        num_col = (len(band_avatar_dict[band]) // col_max_avatars) + 1
+        init_x_coord = band - ((num_col - 1) * horiz_offset * 0.5)
+        for avatar in range(len(band_avatar_dict[band])):
+            x_coord = init_x_coord + horiz_offset * (avatar // col_max_avatars)
+            y_coord = init_vert_offset + (avatar % col_max_avatars) * vert_offset
+            xy = (x_coord, y_coord)
+            avatar_loc = band_avatar_dict[band][avatar]
+            # add xy coord to list of coords for avatar
+            avatar_xy_dict[avatar_loc] = avatar_xy_dict.get(avatar_loc, []) + [xy]
 
-        else:  # > 10 avatars in band
-            for avatar in range(10):
-                xy = (band - 2.5, 5 + avatar*10)  # TODO: replace ints with default offsets
-                avatar_loc = band_avatar_dict[band][avatar]
-                avatar_xy_dict[avatar_loc] = avatar_xy_dict.get(avatar_loc, []) + [xy]
-
-            for avatar in range(len(band_avatar_dict[band] - 10)):
-                avatar_loc = band_avatar_dict[band][avatar + 10]
-                xy = (band + 2.5, 5 + avatar * 10)
-                avatar_xy_dict[avatar_loc] = avatar_xy_dict.get(avatar_loc, []) + [xy]
+        # if len(band_avatar_dict[band]) < col_max_avatars:
+        #     for avatar in range(len(band_avatar_dict[band])):
+        #         xy = (band, init_vert_offset + avatar*vert_offset)
+        #         avatar_loc = band_avatar_dict[band][avatar]
+        #         # add xy coord to list of coords for avatar
+        #         avatar_xy_dict[avatar_loc] = avatar_xy_dict.get(avatar_loc, []) + [xy]
+        #
+        # else:  # > 10 avatars in band
+        #     for avatar in range(col_max_avatars):
+        #         xy = (band - 2.5, init_vert_offset + avatar*vert_offset)  # TODO: replace ints with default offsets
+        #         avatar_loc = band_avatar_dict[band][avatar]
+        #         avatar_xy_dict[avatar_loc] = avatar_xy_dict.get(avatar_loc, []) + [xy]
+        #
+        #     for avatar in range(len(band_avatar_dict[band] - 10)):
+        #         avatar_loc = band_avatar_dict[band][avatar + 10]
+        #         xy = (band + 2.5, init_vert_offset + avatar * vert_offset)
+        #         avatar_xy_dict[avatar_loc] = avatar_xy_dict.get(avatar_loc, []) + [xy]
     return avatar_xy_dict
 
 
 if __name__ == '__main__':
-    test_score_avatar_dict = {1: ['chicken', 'tenders'],
+    test_score_avatar_dict = {1: ['foo', 'spam', 'dead', 'parrot'],
                               5: ['halibut', 'patties'],
-                              11: ['original', 'recipe', 'chicken']}
+                              8: ['original', 'recipe', 'chicken'],
+                              9: ['foo', 'spam', 'dead', 'parrot'],
+                              11: ['halibut', 'patties'],
+                              }
 
-    print(test_score_avatar_dict, generate_avatar_coords(test_score_avatar_dict))
+    test_assign_avatars_to_bands_results = {0: ['foo', 'spam', 'dead', 'parrot', 'halibut', 'patties'],
+                                            10: ['original', 'recipe', 'chicken', 'foo', 'spam', 'dead',
+                                                 'parrot', 'halibut', 'patties'],
+                                            20: [],
+                                            30: [],
+                                            40: [],
+                                            50: [],
+                                            60: [],
+                                            70: [],
+                                            80: [],
+                                            90: [],
+                                            100: [],
+                                            }
+
+    test_assign_avatar_coords_results = {'foo': [(0.0, 5), (10.0, 35)],
+                                         'spam': [(0.0, 15), (10.0, 45)],
+                                         'dead': [(0.0, 25), (10.0, 55)],
+                                         'parrot': [(0.0, 35), (10.0, 65)],
+                                         'halibut': [(0.0, 45), (10.0, 75)],
+                                         'patties': [(0.0, 55), (10.0, 85)],
+                                         'original': [(10.0, 5)],
+                                         'recipe': [(10.0, 15)],
+                                         'chicken': [(10.0, 25)]}
+
+    test_generate_avatar_coords_results = {'foo': [(0.0, 5), (10.0, 35)],
+                                           'spam': [(0.0, 15), (10.0, 45)],
+                                           'dead': [(0.0, 25), (10.0, 55)],
+                                           'parrot': [(0.0, 35), (10.0, 65)],
+                                           'halibut': [(0.0, 45), (10.0, 75)],
+                                           'patties': [(0.0, 55), (10.0, 85)],
+                                           'original': [(10.0, 5)],
+                                           'recipe': [(10.0, 15)],
+                                           'chicken': [(10.0, 25)],
+                                           }
+
+    assert assign_avatars_to_bands(test_score_avatar_dict) == test_assign_avatars_to_bands_results
+    assert assign_avatar_coords(test_assign_avatars_to_bands_results) == test_assign_avatar_coords_results
+    assert generate_avatar_coords(test_score_avatar_dict) == test_generate_avatar_coords_results
+
+    assert assign_avatar_coords(test_assign_avatars_to_bands_results) == generate_avatar_coords(test_score_avatar_dict)
