8bdcc767bdd5e5e28b35303f70bcf82a41e29df0
toonarmycaptain <toonarmycaptain@hotmail.com>
2018-12-10 00:30:40 -0600

    Update setup.py data for alpha release.
    Version number, description

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(name='dionysus_app',
-      # version='0.0.1',
-      description='Avatar graph generator',
+      version='0.1.0-alpha',
+      description='Avatar chart generator',
       author='David Antonini',
       author_email='toonarmycaptain@hotmail.com',
       url='https://github.com/toonarmycaptain/dionysus',
