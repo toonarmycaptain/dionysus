c7a93e96822cb3fbf07dfe4181b17d56d11f55a9
David <toonarmycaptain@hotmail.com>
2018-12-21 07:56:41 -0600

    Add TestMoveFile, implement shutil.rmtree instead of individual os funcs
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- test_suite/test_file_functions.py
+++ test_suite/test_file_functions.py
@@ -1,8 +1,8 @@
-import os
+import os, shutil
 
 from unittest import TestCase
 
-from dionysus_app.file_functions import convert_to_json, copy_file
+from dionysus_app.file_functions import convert_to_json, copy_file, move_file
 
 
 class TestConvertToJson(TestCase):
@@ -34,5 +34,30 @@ class TestCopyFile(TestCase):
 
     def tearDown(self):
         os.remove(self.original_filename)  # remove new file
-        os.remove(self.destination_path)  # remove copy of file
-        os.rmdir(self.new_folder_name)  # remove new dir
+        shutil.rmtree(self.new_folder_name)  # remove new dir
+
+
+class TestMoveFile(TestCase):
+    def setUp(self):
+        self.original_filename = 'just_a_naughty_boy.png'
+        self.new_folder_name = 'not_the_messiah'
+
+        self.original_path = self.original_filename
+        self.destination_path = os.path.join(self.new_folder_name, self.original_filename)
+
+        with open(self.original_filename, 'w+') as test_file:
+                        pass
+
+        os.mkdir(self.new_folder_name)
+
+    def test_move_file(self):
+        assert os.path.exists(self.original_filename)
+        assert not os.path.exists(self.destination_path)
+        move_file(self.original_path, self.destination_path)
+        assert os.path.exists(self.destination_path)
+        assert not os.path.exists(self.original_path)
+
+    def tearDown(self):
+        shutil.rmtree(self.new_folder_name)
+
+# TODO: test moving a directory with files in it.
