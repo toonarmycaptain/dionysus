949f02056e128dc58a0b951190caeddab728cd78
David <toonarmycaptain@hotmail.com>
2018-12-21 09:57:42 -0600

    Move setUp asserts setUp func, use shutil.rmtree for os.remove/os.rmdir.
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- test_suite/test_class_functions.py
+++ test_suite/test_class_functions.py
@@ -1,14 +1,13 @@
 """Test functions in class_functions.py"""
 
 import os
+import shutil
 
 from unittest import TestCase
 
 import definitions
 
 from dionysus_app.class_functions import CLASSLIST_DATA_PATH, copy_avatar_to_app_data, setup_class_data_storage
-
-
 
 
 class TestSetupClassDataStorage(TestCase):
@@ -33,30 +32,28 @@ class TestCopyAvatarToAppData(TestCase):
         # Mock value
         definitions.DEFAULT_CHART_SAVE_FOLDER = '.'
 
-
+        # Setup test class storage,
         setup_class_data_storage(self.test_classlist_name)
         self.test_class_datafolder_path = CLASSLIST_DATA_PATH.joinpath(self.test_classlist_name)
         self.test_class_avatar_subfolder_path = self.test_class_datafolder_path.joinpath('avatars')
         self.test_class_chart_data_subfolder_path = self.test_class_datafolder_path.joinpath('chart_data')
 
-    def test_copy_avatar_to_app_data(self):
+        self.copied_avatar_filepath = self.test_class_avatar_subfolder_path.joinpath(self.copied_avatar_save_filename)
+
         # assert test preconditions met
         assert os.path.exists(self.test_avatar_filename)
         assert os.path.exists(self.test_class_avatar_subfolder_path)
 
-        self.copied_avatar_filepath = self.test_class_avatar_subfolder_path.joinpath(self.copied_avatar_save_filename)
         assert not os.path.exists(self.copied_avatar_filepath)
 
+    def test_copy_avatar_to_app_data(self):
         copy_avatar_to_app_data(self.test_classlist_name, self.test_avatar_filename, self.copied_avatar_save_filename)
         assert os.path.exists(self.copied_avatar_filepath)
 
     def tearDown(self):
         os.remove(self.test_avatar_filename)  # remove test avatar file
 
-        os.remove(self.copied_avatar_filepath)  # remove copied avatar
-        os.rmdir(self.test_class_avatar_subfolder_path)  # remove class_datafolder/avatars
-        os.rmdir(self.test_class_chart_data_subfolder_path)  # remove class_datafolder/chart_data
-        os.rmdir(self.test_class_datafolder_path)  # remove class_datafolder
+        shutil.rmtree(self.test_class_datafolder_path)  # remove class_datafolder
         os.rmdir(self.test_classlist_name)  # remove user_save_charts folder
 
         # Restore definitions.DEFAULT_CHART_SAVE_FOLDER to original value
