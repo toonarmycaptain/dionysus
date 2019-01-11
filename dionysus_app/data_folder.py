e7aed2fa6a1076b57d5fecc8669f5b1926e60218
David <toonarmycaptain@hotmail.com>
2019-01-10 22:50:15 -0600

    Add docstring.
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- dionysus_app/data_folder.py
+++ dionysus_app/data_folder.py
@@ -23,6 +23,20 @@ class DataFolder(Enum):
 
     @staticmethod
     def generate_rel_path(path):
+        """
+        Returns a abs path from relative path.
+        eg for APP_DATA use:
+        APP_DATA_path = DataFolder.generate_rel_path(APP_DATA.value)
+
+        If None is passed as an argument, returns the Path of the
+        current working directory.
+
+        If a path is given, the ROOT_DIR of the project is inserted
+        before it.
+
+        :param path: string or None.
+        :return: Path object.
+        """
         if not path:
             return Path.cwd().as_uri()
 
