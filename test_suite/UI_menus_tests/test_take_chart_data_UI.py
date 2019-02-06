834a42b591c6fcbea7dde97b5f4a1ba9505d3939
David <toonarmycaptain@hotmail.com>
2019-02-06 11:34:30 -0600

    Add test for take_student_scores.
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- test_suite/UI_menus_tests/test_take_chart_data_UI.py
+++ test_suite/UI_menus_tests/test_take_chart_data_UI.py
@@ -1,4 +1,4 @@
-from unittest import mock, TestCase
+from unittest import TestCase, mock
 from unittest.mock import patch
 
 from dionysus_app.UI_menus.chart_generator.take_chart_data_UI import (take_score_data,
@@ -31,16 +31,49 @@ class TestTakeScoreData(TestCase):
         mocked_take_student_scores.called_once_with(self.test_classname, self.test_class_data_dict)
 
 
+class TestTakeStudentScores(TestCase):
+    mock_DEFAULT_AVATAR_PATH = 'mocked_default_avatar_path'
 
+    def setUp(self):
+        self.mock_DEFAULT_AVATAR_PATH = 'mocked_default_avatar_path'
 
+        self.test_classname = 'the knights who say ni'
+        self.test_class_data_dict = test_class_data['loaded_dict']
 
+        # NB If data other than avatar in dict values, this test implementation may need to change.
+        self.mocked_get_avatar_path_return_values = [f'path to {avatar[0]}' if avatar[0] is not None
+                                                     else self.mock_DEFAULT_AVATAR_PATH
+                                                     for avatar in test_class_data['loaded_dict'].values()]
 
+        # Gives no score to one student with and without an avatar.
+        self.mocked_take_score_entry_return_values = [0, 1, 3, None, 50, 99, 100, 1, 2, 3, 4, None, 6, 7, 8]
+        self.test_take_student_scores_return_value = {0: ['path to Cali_avatar.png'],
+                                                      1: [self.mock_DEFAULT_AVATAR_PATH, self.mock_DEFAULT_AVATAR_PATH],
+                                                      3: [self.mock_DEFAULT_AVATAR_PATH, self.mock_DEFAULT_AVATAR_PATH],
+                                                      # None: ['Zach_avatar.png', None],  # No score not returned.
+                                                      50: [self.mock_DEFAULT_AVATAR_PATH],
+                                                      99: [self.mock_DEFAULT_AVATAR_PATH],
+                                                      100: [self.mock_DEFAULT_AVATAR_PATH],
+                                                      2: ['path to Ashley_avatar.png'],
+                                                      4: [self.mock_DEFAULT_AVATAR_PATH],
+                                                      6: ['path to Danielle.png'],
+                                                      7: [self.mock_DEFAULT_AVATAR_PATH],
+                                                      8: [self.mock_DEFAULT_AVATAR_PATH]
+                                                      }
 
+    @patch('dionysus_app.UI_menus.chart_generator.take_chart_data_UI.get_avatar_path')
+    @patch('dionysus_app.UI_menus.chart_generator.take_chart_data_UI.take_score_entry')
+    def test_take_student_scores(self, mocked_take_score_entry, mocked_get_avatar_path):
+        mocked_take_score_entry.side_effect = self.mocked_take_score_entry_return_values
+        mocked_get_avatar_path.side_effect = self.mocked_get_avatar_path_return_values
 
+        assert take_student_scores(self.test_classname,
+                                   self.test_class_data_dict) == self.test_take_student_scores_return_value
 
-
-
-
+        mocked_take_score_entry.call_args_list = [mock.call(student_name) for student_name in self.test_class_data_dict]
+        mocked_get_avatar_path.call_args_list = [mock.call(self.test_classname,
+                                                           self.test_class_data_dict[student_name][0])
+                                                 for student_name in self.test_class_data_dict]
 
 
 def test_take_score_entry():
