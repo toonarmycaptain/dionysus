044536691f974d34c9fbdee22fdf489d7c8fab6e
David <toonarmycaptain@hotmail.com>
2019-02-20 12:22:59 -0600

    PEP8 cleanup.
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- test_suite/UI_menus_tests/test_settings_menu.py
+++ test_suite/UI_menus_tests/test_settings_menu.py
@@ -4,14 +4,12 @@ from unittest import TestCase, mock
 from unittest.mock import patch
 
 from dionysus_app.UI_menus.settings_menu import (run_settings_menu,
-                                    )
+                                                 )
 
 
 class TestRunSettingsMenu(TestCase):
     def setUp(self):
         self.return_to_main_menu_input = '0'
-
-
 
     @patch('dionysus_app.UI_menus.settings_menu.take_settings_menu_input')
     @patch('dionysus_app.UI_menus.settings_menu.settings_menu_options')
@@ -20,7 +18,7 @@ class TestRunSettingsMenu(TestCase):
                                mocked_take_settings_menu_input
                                ):
 
-        mocked_take_settings_menu_input_returns = [None, None, True] # 2 mock feature calls, return to main menu.
+        mocked_take_settings_menu_input_returns = [None, None, True]  # 2 mock feature calls, return to main menu.
         mocked_take_settings_menu_input.side_effect = mocked_take_settings_menu_input_returns
 
         assert run_settings_menu() is None
@@ -28,9 +26,6 @@ class TestRunSettingsMenu(TestCase):
         main_menu_calls = [mock.call() for each_call in mocked_take_settings_menu_input_returns]
         assert mocked_settings_menu_options.call_args_list == main_menu_calls
         assert mocked_take_settings_menu_input.call_args_list == main_menu_calls
-
-
-
 
 
 class TestTakeSettingsMenuInput(TestCase):
@@ -52,21 +47,3 @@ class TestTakeSettingsMenuInput(TestCase):
                                ]
 
         self.final_valid_input = self.return_to_main_menu_input
-"""
-
-    possible_options = {
-        '1': call_set_default_chart_save_location,
-        '0': return_to_main_menu,
-        }
-
-    while True:
-        chosen_option = input('>>> ')
-
-        if chosen_option in possible_options:
-            possible_options[chosen_option]()
-            break  # Exit loop when chosen action finishes. Returns None.
-        if chosen_option == '0':  # User selects to return to main menu.
-            return True
-        # else:
-        print("Invalid input.")
-"""
