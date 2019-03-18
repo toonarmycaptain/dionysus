4ea7a13bed20a1745142d53551e72d092b54c169
David <toonarmycaptain@hotmail.com>
2019-03-18 13:37:19 -0500

    Fix typo.
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- test_suite/test_student.py
+++ test_suite/test_student.py
@@ -6,7 +6,9 @@ from dionysus_app.student import Student
 
 class TestStudent(TestCase):
     def setUp(self):
-        self.test_name = 'Arther, King of the Britons'
+        self.test_name = 'Arthur, King of the Britons'
+        self.test_path_safe_name = 'Arthur_King_of_the_Britons'
+
         self.test_student = Student(self.test_name)
 
     def test_name_method(self):
