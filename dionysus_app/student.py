39d97c07d2939b37f20e5fea7167980c9b4643fb
David <toonarmycaptain@hotmail.com>
2019-03-18 13:39:04 -0500

    Add path_safe_name attribute. Implement @property for attributes, tests.
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- dionysus_app/student.py
+++ dionysus_app/student.py
@@ -1,5 +1,26 @@
 """Class for student."""
 
+from dionysus_app.UI_menus.UI_functions import clean_for_filename
+
+
 class Student:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
+        self.path_safe_name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+        self.path_safe_name = self._name
+
+    @property
+    def path_safe_name(self):
+        return self._path_safe_name
+
+    @path_safe_name.setter
+    def path_safe_name(self, path_safe_name):
+        self._path_safe_name = clean_for_filename(path_safe_name)
--- test_suite/test_student.py
+++ test_suite/test_student.py
@@ -11,5 +11,22 @@ class TestStudent(TestCase):
 
         self.test_student = Student(self.test_name)
 
+        self.test_changed_name = 'Sir Lancelot: the not-so-brave'
+        self.test_changed_path_safe_name = 'Sir_Lancelot_the_not-so-brave'
+
     def test_name_method(self):
-        assert self.test_student.name is self.test_name
+        assert self.test_student.name == self.test_name
+
+    def test_path_safe_name_method(self):
+        assert self.test_student.path_safe_name == self.test_path_safe_name
+
+    def test_change_name(self):
+        assert self.test_student.name != self.test_changed_name
+        assert self.test_student.path_safe_name != self.test_changed_path_safe_name
+
+        # Change name
+        self.test_student.name = self.test_changed_name
+
+        assert self.test_student.name == self.test_changed_name
+        #  Assert name change carried over to path_safe_name attribute.
+        assert self.test_student.path_safe_name == self.test_changed_path_safe_name
