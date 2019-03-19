a3b382cbe9623b0b7b0d936cf08af589d611b30f
David <toonarmycaptain@hotmail.com>
2019-03-19 12:46:06 -0500

    Write docstrings. Add typehints, avatar_path attribute, tests.
    Signed-off-by: David <toonarmycaptain@hotmail.com>

--- dionysus_app/student.py
+++ dionysus_app/student.py
@@ -1,26 +1,94 @@
 """Class for student."""
 
+from pathlib import Path
+from typing import Union
+
 from dionysus_app.UI_menus.UI_functions import clean_for_filename
 
 
 class Student:
-    def __init__(self, name: str):
+    """
+    Class for student objects.
+    ...
+
+    Attributes
+    ----------
+    :attr name: str Student's name
+    :attr path_safe_name: str
+    :attr avatar: Path or None - Path to student's avatar.
+
+    Methods
+    -------
+
+    """
+
+    def __init__(self, name: str, avatar_path: Union[Path, str] = None):
+
         self.name = name
         self.path_safe_name = name
 
+        self.avatar_path = avatar_path
+        # NB Assuring existence is responsibility of code instantiating/adding avatar_path.
+
     @property
     def name(self):
+        """
+        Get student name.
+
+        :return: str
+        """
         return self._name
 
     @name.setter
-    def name(self, name):
+    def name(self, name: str):
+        """
+        Set student name. Also sets path_safe_name.
+
+        :param name: str
+        :return: None
+        """
         self._name = name
         self.path_safe_name = self._name
 
     @property
     def path_safe_name(self):
+        """
+        Get path_safe_name: filename-safe student name string.
+
+        :return: str
+        """
         return self._path_safe_name
 
     @path_safe_name.setter
-    def path_safe_name(self, path_safe_name):
-        self._path_safe_name = clean_for_filename(path_safe_name)
+    def path_safe_name(self, student_name: str):
+        """
+        Set path_safe_name: filename-safe student name string.
+
+        :param student_name: str
+        :return: None
+        """
+        self._path_safe_name = clean_for_filename(student_name)
+
+    @property
+    def avatar_path(self):
+        """
+        Get avatar path.
+
+        :return: Path or None
+        """
+        return self._avatar
+
+    @avatar_path.setter
+    def avatar_path(self, avatar_path: Union[Path, str] = None):
+        """
+        Set _avatar_path. Ensure saved value is Path object, otherwise None.
+
+        :param avatar_path: Path, str, or None.
+        :return: None
+        """
+        self._avatar: Union[Path, None]
+        if avatar_path:
+
+            self._avatar = Path(avatar_path)
+        else:
+            self._avatar = None
--- test_suite/test_student.py
+++ test_suite/test_student.py
@@ -1,5 +1,7 @@
 """Tests for Student class."""
-from unittest import mock, TestCase
+from pathlib import Path
+from unittest import TestCase
+from unittest.mock import patch
 
 from dionysus_app.student import Student
 
@@ -14,13 +16,16 @@ class TestStudent(TestCase):
         self.test_changed_name = 'Sir Lancelot: the not-so-brave'
         self.test_changed_path_safe_name = 'Sir_Lancelot_the_not-so-brave'
 
-    def test_name_method(self):
+        self.test_avatar_path_str = 'camelot'
+        self.test_avatar_path_path = Path('camelot')
+
+    def test_name_getter(self):
         assert self.test_student.name == self.test_name
 
-    def test_path_safe_name_method(self):
+    def test_path_safe_name_getter(self):
         assert self.test_student.path_safe_name == self.test_path_safe_name
 
-    def test_change_name(self):
+    def test_name_setter_unmocked(self):
         assert self.test_student.name != self.test_changed_name
         assert self.test_student.path_safe_name != self.test_changed_path_safe_name
 
@@ -30,3 +35,54 @@ class TestStudent(TestCase):
         assert self.test_student.name == self.test_changed_name
         #  Assert name change carried over to path_safe_name attribute.
         assert self.test_student.path_safe_name == self.test_changed_path_safe_name
+
+    @patch('dionysus_app.student.clean_for_filename')
+    def test_name_setter_mocked(self, mocked_clean_for_filename):
+        assert self.test_student.name != self.test_changed_name
+        assert self.test_student.path_safe_name != self.test_changed_path_safe_name
+
+        mocked_clean_for_filename.return_value = self.test_changed_path_safe_name
+
+        # Change name
+        self.test_student.name = self.test_changed_name
+
+        assert self.test_student.name == self.test_changed_name
+        #  Assert name change carried over to path_safe_name attribute.
+        assert self.test_student.path_safe_name == self.test_changed_path_safe_name
+
+        mocked_clean_for_filename.assert_called_once_with(self.test_changed_name)
+
+    def test_no_avatar_on_instantiation(self):
+        assert self.test_student.avatar_path is None
+
+    def test_change_avatar_passing_str(self):
+        assert self.test_student.avatar_path != self.test_avatar_path_path
+
+        self.test_student.avatar_path = self.test_avatar_path_path
+
+        assert isinstance(self.test_student.avatar_path, Path)
+        assert self.test_student.avatar_path == self.test_avatar_path_path
+
+    def test_change_avatar_passing_path(self):
+        assert self.test_student.avatar_path != self.test_avatar_path_path
+
+        self.test_student.avatar_path = self.test_avatar_path_path
+
+        assert isinstance(self.test_student.avatar_path, Path)
+        assert self.test_student.avatar_path == self.test_avatar_path_path
+
+    def test_instantiate_with_avatar_path_str(self):
+        test_instantiate_student_with_avatar = Student(self.test_name,
+                                                       self.test_avatar_path_str)
+
+        assert test_instantiate_student_with_avatar.avatar_path == self.test_avatar_path_path
+
+        assert isinstance(test_instantiate_student_with_avatar.avatar_path, Path)
+
+    def test_instantiate_with_avatar_path_path(self):
+        test_instantiate_student_with_avatar = Student(self.test_name,
+                                                       self.test_avatar_path_path)
+
+        assert test_instantiate_student_with_avatar.avatar_path == self.test_avatar_path_path
+
+        assert isinstance(test_instantiate_student_with_avatar.avatar_path, Path)
