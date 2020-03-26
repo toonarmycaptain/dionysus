1ac06fc680d41d3745e2c1dcc857c7607213a82c
toonarmycaptain <toonarmycaptain@hotmail.com>
2020-03-25 23:37:57 -0500

    Convert TestClassNamePathSafeName to pytest style tests.
    Signed-off-by: toonarmycaptain <toonarmycaptain@hotmail.com>

--- test_suite/test_class.py
+++ test_suite/test_class.py
@@ -1,9 +1,7 @@
 """Tests for class.py"""
 import pytest
 
-from unittest import TestCase
-from unittest.mock import patch
-
+from dionysus_app import class_
 from dionysus_app.class_ import Class
 from dionysus_app.file_functions import convert_to_json
 from dionysus_app.student import Student
@@ -55,65 +53,62 @@ def test_class_student_list_instantiation_no_list_arg():
 
 
 class TestClassNamePathSafeName:
-    """
-    Test Class name and path_safe_name properties.
-    """
+    """Test Class name and path_safe_name properties."""
 
-    def setup_method(self):
-        """
-        Setup class name and name for change.
-        """
+    def test_name_getter(self):
+        test_name = "The Knights of the Round-table: we don't say 'Ni!'"
+        assert Class(test_name).name == test_name
 
-        self.test_name = "The Knights of the Round-table: we don't say 'Ni!'"
-        self.test_path_safe_name = "The_Knights_of_the_Round-table__we_don_t_say__Ni__"
+    def test_path_safe_name_getter(self):
+        assert Class("The Knights of the Round-table: we don't say 'Ni!'"
+                     ).path_safe_name == "The_Knights_of_the_Round-table__we_don_t_say__Ni__"
 
-        self.test_changed_name = 'Adaptable Knights: We now say Ni!, but we dont have to.'
-        self.test_changed_path_safe_name = 'Adaptable_Knights__We_now_say_Ni___but_we_dont_have_to_'
+    def test_path_safe_name_getter_mocking_calls(self, monkeypatch):
+        mock_path_safe_name = "Something completely different."
 
-    def test_name_getter(self, test_class_name_only):
-        assert test_class_name_only.name == self.test_name
+        def mocked_clean_for_filename(class_name):
+            return mock_path_safe_name
 
-    def test_path_safe_name_getter(self, test_class_name_only):
-        assert test_class_name_only.path_safe_name == self.test_path_safe_name
+        monkeypatch.setattr(class_, 'clean_for_filename', mocked_clean_for_filename)
 
-    def test_name_setter_unmocked(self, test_class_name_only):
-        assert test_class_name_only.name != self.test_changed_name
-        assert test_class_name_only.path_safe_name != self.test_changed_path_safe_name
+        assert Class("The Knights of the Round-table: we don't say 'Ni!'").path_safe_name == mock_path_safe_name
 
-        # Change name
-        test_class_name_only.name = self.test_changed_name
+    def test_name_setter(self):
+        test_name = "The Knights of the Round-table: we don't say 'Ni!'"
 
-        assert test_class_name_only.name == self.test_changed_name
-        #  Assert name change carried over to path_safe_name attribute.
-        assert test_class_name_only.path_safe_name == self.test_changed_path_safe_name
+        test_changed_name = 'Adaptable Knights: We now say Ni!, but we dont have to.'
+        test_changed_path_safe_name = 'Adaptable_Knights__We_now_say_Ni___but_we_dont_have_to_'
 
+        test_class = Class(test_name)
 
-class TestClassNameMocked(TestCase):
-    def setUp(self):
-        self.test_name = "The Knights of the Round-table: we don't say 'Ni!'"
-        self.test_path_safe_name = "The_Knights_of_the_Round-table_we_don't_say__Ni__"
-
-        self.test_class = Class(self.test_name)
-
-        self.test_changed_name = 'Adaptable Knights: We now say Ni!, but we dont have to.'
-        self.test_changed_path_safe_name = 'Adaptable_Knights__We_now_say_Ni___but_we_dont_have_to_'
-
-    @patch('dionysus_app.class_.clean_for_filename')
-    def test_name_setter_mocked(self, mocked_clean_for_filename):
-        # Assert name, path_safe_name initial value != changed value
-        assert self.test_class.name != self.test_changed_name
-        assert self.test_class.path_safe_name != self.test_changed_path_safe_name
-
-        mocked_clean_for_filename.return_value = self.test_changed_path_safe_name
+        # Original test_class attributes not equal to changed:
+        assert (test_class.name, test_class.path_safe_name) != (test_changed_name, test_changed_path_safe_name)
 
         # Change name
-        self.test_class.name = self.test_changed_name
+        test_class.name = test_changed_name
 
-        assert self.test_class.name == self.test_changed_name
-        #  Assert name change carried over to path_safe_name attribute.
-        assert self.test_class.path_safe_name == self.test_changed_path_safe_name
+        assert (test_class.name, test_class.path_safe_name) == (test_changed_name, test_changed_path_safe_name)
 
-        mocked_clean_for_filename.assert_called_once_with(self.test_changed_name)
+    def test_name_setter_mocking_calls(self, monkeypatch):
+        test_name = "The Knights of the Round-table: we don't say 'Ni!'"
+
+        test_changed_name = 'Adaptable Knights: We now say Ni!, but we dont have to.'
+        mock_changed_path_safe_name = "Adaptable_Knights: We're Niiiearly completely un!safe?!$"
+
+        test_class = Class(test_name)
+
+        # Original test_class attributes not equal to changed:
+        assert (test_class.name, test_class.path_safe_name) != (test_changed_name, mock_changed_path_safe_name)
+
+        def mocked_clean_for_filename(class_name):
+            return mock_changed_path_safe_name
+
+        monkeypatch.setattr(class_, 'clean_for_filename', mocked_clean_for_filename)
+
+        # Change name
+        test_class.name = test_changed_name
+
+        assert (test_class.name, test_class.path_safe_name) == (test_changed_name, mock_changed_path_safe_name)
 
 
 class TestContainsMethod:
