2927a3a271c601c3b14ffd51d64fc09b6ceed0a3
toonarmycaptain <toonarmycaptain@hotmail.com>
2020-06-24 23:11:30 -0500

    REGISTRY replaced with DATABASE var for database object.
    DATABASE object accessed via import definitions; definitions.DATABASE.
    
    DEFAULT_DATABASE_BACKEND var used as fallback to choose a database
    backend, if user neglects to choose.
    
    Signed-off-by: toonarmycaptain <toonarmycaptain@hotmail.com>

--- definitions.py
+++ definitions.py
@@ -1,7 +1,20 @@
+"""
+definitions.py - source for vars used throughout application.None
+
+ROOT_DIR - path to directory containing app_main/definitions
+DEFAULT_DATABASE_BACKEND - the default database backend
+DATABASE - the database object.
+DEFAULT_CHART_SAVE_DIR - default user save folder for generated charts.
+
+For state-holding object eg DATABASE, must import definitions, then use
+dot access to use the object:
+    import definitions
+    definitions.DATABASE.do_stuff()
+"""
 import os
 
 from pathlib import Path
-from typing import List, Optional, TYPE_CHECKING
+from typing import Optional, TYPE_CHECKING
 
 # Import to get around circular import caused by type checking. Type as string.
 if TYPE_CHECKING:
@@ -10,8 +23,8 @@ if TYPE_CHECKING:
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # Global root directory.
 
 DEFAULT_DATABASE_BACKEND = 'JSON'
+# Ignore typehint error: DATABASE object needs to be initialised with a value
+DATABASE: 'Database' = None # type: ignore
 
-DATABASE: Optional['Database'] = None
-REGISTRY: Optional[List] = None
 
-DEFAULT_CHART_SAVE_FOLDER: Optional[Path] = None  # Path object.
+DEFAULT_CHART_SAVE_DIR: Optional[Path] = None  # Path object.
