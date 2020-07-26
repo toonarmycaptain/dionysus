3fbb74e5480adfe6d3ab3c730ccfc75e6ab22d78
David Antonini <toonarmycaptain@hotmail.com>
2020-07-26 12:26:35 -0500

    Begin first pass at SQLite database implementation, tests.
    Except type checking errors on as-yet unimplemented methods.
    
    Signed-off-by: David Antonini <toonarmycaptain@hotmail.com>

---
+++ dionysus_app/persistence/databases/sqlite.py
@@ -0,0 +1,163 @@
+import sqlite3
+
+from pathlib import Path
+from sqlite3 import Error
+from typing import Any, List, Optional
+
+# noinspection PyPackageRequirements
+import matplotlib.pyplot as plt
+
+from dionysus_app.class_ import Class, NewClass
+from dionysus_app.data_folder import DataFolder
+from dionysus_app.student import Student
+from dionysus_app.persistence.database import ClassIdentifier, Database
+
+
+class SQLiteDatabase(Database):
+    def __init__(self, default_avatar_path: Path = None,
+                 database_path: Path = None,
+                 ):
+        self.database_path: Path = (
+                database_path
+                or DataFolder.generate_rel_path(DataFolder.APP_DATA.value).joinpath('dionysus.db'))
+        self.default_avatar_path: Path = (
+                default_avatar_path
+                or DataFolder.generate_rel_path(DataFolder.DEFAULT_AVATAR.value))
+        # check if db file exists/db has appropriate tables etc
+        self._init_db()
+
+    def get_classes(self) -> List[ClassIdentifier]:
+        """
+        Return list of ClassIdentifiers for classes in the database.
+
+        :return: List[ClassIdentifier]
+        """
+        with self._connection() as conn:
+            # get list of tuple/list pairs of id, name
+            classes = conn.cursor().execute("""SELECT id, name FROM class;""").fetchall()
+            # convert to ClassIdentifiers
+            return [ClassIdentifier(*class_data) for class_data in classes]
+
+    def class_name_exists(self, class_name: str) -> bool:
+        with self._connection() as conn:
+            matching_class = conn.cursor().execute("""SELECT class.name FROM class 
+                                                   WHERE name=? LIMIT 1""", (class_name,))
+            # Will return either no class [] or [('class name',)] if matching class name found.
+            return bool(matching_class.fetchone())
+
+    def create_class(self, new_class: NewClass) -> None:
+        with self._connection() as conn:
+            cursor = conn.cursor()
+            # insert class into class
+            cursor.execute("""INSERT INTO class(name) VALUES(?)""", (new_class.name,))
+            # Add id to class:
+            new_class.id = cursor.lastrowid
+            for student in new_class:
+                # insert student
+                cursor.execute(
+                        """INSERT INTO student(name, class_id, avatar) VALUES(?,?,?)""",
+                        (student.name, new_class.id, student.avatar_id))
+                # Add id to student:
+                student.id = cursor.lastrowid
+            conn.commit()
+        conn.close()
+
+    def load_class(self, class_id: int) -> Class:
+        with self._connection() as conn:
+            # Get class from db, use 'loaded_class_id' to avoid name clash with class_id when loading student.
+            loaded_class_id, class_name = conn.cursor().execute(
+                    """SELECT * FROM class WHERE class.id=?""", (class_id,)).fetchone()
+            # Get student data for class:
+            students_data = conn.cursor().execute(
+                    """SELECT * FROM student WHERE student.class_id=?""", (loaded_class_id,)).fetchall()
+            # Instantiate student objects:
+            students_list = [Student(id=id, name=name, class_id=class_id, avatar_id=avatar)
+                             for student_id, name, class_id, avatar in students_data]
+        conn.close()
+
+        return Class(id=loaded_class_id, name=class_name, students=students_list)
+
+    def update_class(self, class_to_write: Class) -> None:
+        return NotImplementedError  # type: ignore
+
+    def get_avatar_path(self, avatar_id: Any) -> Path:
+        return NotImplementedError  # type: ignore
+
+    def create_chart(self, chart_data_dict: dict) -> None:
+        return NotImplementedError  # type: ignore
+
+    def save_chart_image(self, chart_data_dict: dict, mpl_plt: plt) -> Path:
+        return NotImplementedError  # type: ignore
+
+    def close(self) -> None:
+        return NotImplementedError  # type: ignore
+
+    def _connection(self) -> sqlite3.Connection:
+
+        connection = sqlite3.connect(self.database_path)
+        # Ensure foreign key constraint enforcement.
+        connection.cursor().execute("""PRAGMA foreign_keys=ON;""")
+        # print("Connection to SQLite DB successful")
+        return connection
+        # handle case where connection fails?
+        # or should it fail, since on disk db connection should not fail?
+
+    def _init_db(self):
+        """Create empty database.
+        Function could have better name."""
+        table_creation_functions = [self._create_table_class,
+                                    self._create_table_student,
+                                    self._create_table_chart,
+                                    self._create_table_score,
+                                    ]
+
+        connection = self._connection()
+
+        for create_table_query in table_creation_functions:
+            connection.cursor().execute(create_table_query())
+            connection.commit()
+        connection.close()
+
+    def _create_table_class(self) -> str:
+        return """CREATE TABLE IF NOT EXISTS class(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        -- Ensure name is type text no longer than 255 characters: 
+                        name TEXT NOT NULL CHECK(typeof("name") = 'text' AND
+                                                 length("name") <= 255
+                                                 )
+                        );
+                        """
+
+    def _create_table_student(self) -> str:
+        return """CREATE TABLE IF NOT EXISTS student(
+                        -- primary key must be INTEGER not INT, NOT NULL is implicit, or error/autoincrement won't work.
+                        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                        name TEXT NOT NULL CHECK(typeof("name") = 'text' AND
+                                                 length("name") <= 255
+                                                 ),
+                        class_id INTEGER,
+                        avatar BLOB,
+                        FOREIGN KEY (class_id) REFERENCES class (id)
+                        );
+                        """
+
+    def _create_table_chart(self) -> str:
+        return """CREATE TABLE IF NOT EXISTS chart(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT NOT NULL CHECK(typeof("name") = 'text' AND
+                                                 length("name") <= 255
+                                                 ),
+                        date TEXT -- For now can be NULL, will be implemented later.
+                        );
+                        """
+
+    def _create_table_score(self) -> str:
+        return """CREATE TABLE IF NOT EXISTS score(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        chart_id INTEGER NOT NULL,
+                        student_id INTEGER NOT NULL,
+                        value REAL NOT NULL, --REAL is equivalent of float.
+                        FOREIGN KEY(chart_id) REFERENCES chart(id),
+                        FOREIGN KEY(student_id) REFERENCES student(id)
+                        );
+                        """
---
+++ test_suite/test_persistence/test_databases/test_sqlite.py
@@ -0,0 +1,153 @@
+import sqlite3
+
+from random import randint
+
+import pytest
+
+from dionysus_app.class_ import Class, NewClass
+from dionysus_app.persistence.database import ClassIdentifier
+from dionysus_app.persistence.databases.sqlite import SQLiteDatabase
+
+from test_suite.test_class import test_class_name_only, test_full_class  # Fixture imports.
+
+
+def empty_sqlite_test_db(db_path):
+    # give random db ref to avoid having to drop the tables and recreate each time....much faster!
+    num = randint(1, 1000000000)
+    # for some reason this didn't work on linux, but does on Win10.
+    # return SQLiteDatabase(database_path=f'file:test_db{num}?mode=memory&cache=shared')
+    # Slower, but cross platform:
+    return SQLiteDatabase(database_path=f'{db_path}/test_db{num}?mode=memory&cache=shared')
+
+
+@pytest.fixture
+def empty_sqlite_database(tmpdir):
+    """return in-RAM empty SQLiteDatabase.
+
+    Basing in tmpdir works where basing in a 'file:' fails on linux,
+    and also ensures test atomicity, each test starting with a fresh,
+    empty database.
+    """
+    test_db = empty_sqlite_test_db(tmpdir)
+    return test_db
+
+
+def test_empty_sqlite_database_fixture(empty_sqlite_database):
+    """Ensure test db can be connected to and tables exist."""
+    assert empty_sqlite_database
+    tables = ['class', 'student', 'chart', 'score']
+    test_db_tables = empty_sqlite_database._connection().execute(
+            """SELECT name from sqlite_master WHERE type='table' """).fetchall()
+    for table in tables:
+        assert (table,) in test_db_tables
+
+
+class TestGetClasses:
+    @pytest.mark.parametrize(
+            'existing_class_names, returned_value',
+            [([], []),
+             (['one class'], [ClassIdentifier(id=1, name='one class')]
+              ),
+             (['one class', 'another'], [ClassIdentifier(id=1, name='one class'),
+                                         ClassIdentifier(id=2, name='another')]),
+             (['one class', 'another', 'so many'], [ClassIdentifier(id=1, name='one class'),
+                                                    ClassIdentifier(id=2, name='another'),
+                                                    ClassIdentifier(id=3, name='so many')]),
+             pytest.param(['tricky'], ['tricky'], marks=pytest.mark.xfail(reason='Wrong format return value.')),
+             pytest.param(['one class', 'another', 'so many'], [ClassIdentifier(id=1, name='one class'),
+                                                                ClassIdentifier(id=2, name='another'),
+                                                                ClassIdentifier(id=3, name='wrong')],
+                          marks=pytest.mark.xfail(reason='Inconsistent class list.')),
+             pytest.param(['one class', 'another', 'so many'], [ClassIdentifier(id=1, name='one class'),
+                                                                ClassIdentifier(id=2, name='another'),
+                                                                ClassIdentifier(id=3, name='so many'),
+                                                                ClassIdentifier(id=4, name='wrong')],
+                          marks=pytest.mark.xfail(reason='Inconsistent class list - extra class.')),
+             pytest.param(['one class', 'another', 'so many'], [ClassIdentifier(id=1, name='one class'),
+                                                                ClassIdentifier(id=2, name='another')],
+                          marks=pytest.mark.xfail(reason='Inconsistent class list - missing class.')),
+             ])
+    def test_get_classes(self, empty_sqlite_database,
+                         existing_class_names, returned_value):
+        test_sqlite_database = empty_sqlite_database
+        for class_name in existing_class_names:
+            empty_sqlite_database.create_class(NewClass(name=class_name))
+
+        assert test_sqlite_database.get_classes() == returned_value
+
+
+class TestClassNameExists:
+    @pytest.mark.parametrize(
+            'test_class_name, existing_class_names, returned_value',
+            [('one class', ['one class'], True),
+             ('one class', ['one class', 'another'], True),
+             ('one class', ['one class', 'another', 'so many'], True),
+             ('nonexistent class', [], False),
+             ('nonexistent class', ['one class'], False),
+             ('nonexistent class', ['one class', 'another'], False),
+             ('nonexistent class', ['one class', 'another', 'so many'], False),
+             pytest.param('one class', [], True,
+                          marks=pytest.mark.xfail(reason='Class does not actually exist.')),
+             pytest.param('one class', ['tricky'], True,
+                          marks=pytest.mark.xfail(reason='Class does not actually exist.')),
+             pytest.param('one_class', ['one_class'], False,
+                          marks=pytest.mark.xfail(reason='Class does exist.')),
+             ])
+    def test_class_name_exists(self, empty_sqlite_database,
+                               test_class_name, existing_class_names, returned_value):
+        test_database = empty_sqlite_database
+        for class_name in existing_class_names:
+            test_database.create_class(NewClass(name=class_name))
+
+        assert test_database.class_name_exists(test_class_name) == returned_value
+
+
+class TestCreateClass:
+    @pytest.mark.parametrize('class_data', ['test_class_name_only', 'test_full_class'])
+    def test_create_class(self, request, empty_sqlite_database, class_data):
+        test_database = empty_sqlite_database
+        test_class_data = request.getfixturevalue(class_data)
+        test_class = NewClass.from_dict(test_class_data.json_dict())
+
+        # no students or class in empty db:
+        assert not test_database._connection().cursor().execute("""SELECT * FROM class""").fetchall()
+        assert not test_database._connection().cursor().execute("""SELECT * FROM student""").fetchall()
+
+        # Create class in db:
+        assert test_database.create_class(test_class) is None
+
+        # Find class id, load class to verify:
+        classes = test_database.get_classes()
+        test_class_id = classes[0].id
+
+        assert test_database.load_class(  # NB Returned object will be Class, not NewClass:
+                test_class_id).json_dict() == Class.from_dict(test_class_data.json_dict()).json_dict()
+
+
+class TestLoadClass:
+    @pytest.mark.parametrize('class_data', ['test_class_name_only', 'test_full_class'])
+    def test_create_class(self, request, empty_sqlite_database, class_data):
+        test_database = empty_sqlite_database
+        test_existing_class_data = request.getfixturevalue(class_data)
+        test_existing_class = NewClass.from_dict(test_existing_class_data.json_dict())
+        # Create class in db:
+        test_database.create_class(test_existing_class)
+
+        # Find class id to load:
+        classes = test_database.get_classes()
+        test_full_class_id = classes[0].id
+
+        # Load class, verify data.
+        assert test_database.load_class(  # NB Returned object will be Class, not NewClass:
+                test_full_class_id).json_dict() == Class.from_dict(
+                test_existing_class_data.json_dict()).json_dict()
+
+        assert test_database._connection().cursor().execute("""SELECT * FROM class""").fetchall()
+        if test_existing_class.students:
+            assert test_database._connection().cursor().execute("""SELECT * FROM student""").fetchall()
+
+
+class Test_Connection:
+    def test__connection(self, empty_sqlite_database):
+        """Connection function returns connection."""
+        assert isinstance(empty_sqlite_database._connection(), sqlite3.Connection)
